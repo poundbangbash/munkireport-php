62bb81d5e5fc5d70f60fb4dd215b1a801767f36d
eholtam <poundbangbash@users.noreply.github.com>
2018-02-09 22:32:09 -0600

    Added change() to migration

--- app/modules/usage_stats/migrations/2018_02_09_143049_usage_stats_double_expansion.php
+++ app/modules/usage_stats/migrations/2018_02_09_143049_usage_stats_double_expansion.php
@@ -9,30 +9,30 @@ class UsageStatsDoubleExpansion extends Migration
     {
         $capsule = new Capsule();
         $capsule::schema()->table('usage_stats', function (Blueprint $table) {
-            $table->double('ibyte_rate',16,2)->nullable();
-            $table->double('ibytes',16,2)->nullable();
-            $table->double('ipacket_rate',16,2)->nullable();
-            $table->double('ipackets',16,2)->nullable();
-            $table->double('obyte_rate',16,2)->nullable();
-            $table->double('obytes',16,2)->nullable();
-            $table->double('opacket_rate',16,2)->nullable();
-            $table->double('opackets',16,2)->nullable();
-            $table->double('rbytes_per_s',16,2)->nullable();
-            $table->double('rops_per_s',16,2)->nullable();
-            $table->double('wbytes_per_s',16,2)->nullable();
-            $table->double('wops_per_s',16,2)->nullable();
-            $table->double('rbytes_diff',16,2)->nullable();
-            $table->double('rops_diff',16,2)->nullable();
-            $table->double('wbytes_diff',16,2)->nullable();
-            $table->double('wops_diff',16,2)->nullable();
-            $table->double('package_watts',16,2)->nullable();
-            $table->double('package_joules',16,2)->nullable();
-            $table->double('freq_hz',16,2)->nullable(); // CPU
-            $table->double('freq_ratio',16,2)->nullable(); // CPU
-            $table->double('gpu_freq_hz',16,2)->nullable();
-            $table->double('gpu_freq_mhz',16,2)->nullable();
-            $table->double('gpu_freq_ratio',16,2)->nullable();
-            $table->double('gpu_busy',16,2)->nullable();
+            $table->double('ibyte_rate',16,2)->nullable()->change();
+            $table->double('ibytes',16,2)->nullable()->change();
+            $table->double('ipacket_rate',16,2)->nullable()->change();
+            $table->double('ipackets',16,2)->nullable()->change();
+            $table->double('obyte_rate',16,2)->nullable()->change();
+            $table->double('obytes',16,2)->nullable()->change();
+            $table->double('opacket_rate',16,2)->nullable()->change();
+            $table->double('opackets',16,2)->nullable()->change();
+            $table->double('rbytes_per_s',16,2)->nullable()->change();
+            $table->double('rops_per_s',16,2)->nullable()->change();
+            $table->double('wbytes_per_s',16,2)->nullable()->change();
+            $table->double('wops_per_s',16,2)->nullable()->change();
+            $table->double('rbytes_diff',16,2)->nullable()->change();
+            $table->double('rops_diff',16,2)->nullable()->change();
+            $table->double('wbytes_diff',16,2)->nullable()->change();
+            $table->double('wops_diff',16,2)->nullable()->change();
+            $table->double('package_watts',16,2)->nullable()->change();
+            $table->double('package_joules',16,2)->nullable()->change();
+            $table->double('freq_hz',16,2)->nullable()->change(); // CPU
+            $table->double('freq_ratio',16,2)->nullable()->change(); // CPU
+            $table->double('gpu_freq_hz',16,2)->nullable()->change();
+            $table->double('gpu_freq_mhz',16,2)->nullable()->change();
+            $table->double('gpu_freq_ratio',16,2)->nullable()->change();
+            $table->double('gpu_busy',16,2)->nullable()->change();
         });
     }
     
