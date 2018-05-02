5da0c34eed4975eff7180a3235ac02224bbe8443
eholtam <poundbangbash@users.noreply.github.com>
2018-05-02 17:02:45 -0500

    add equals

--- app/modules/sentinelone/sentinelone_model.php
+++ app/modules/sentinelone/sentinelone_model.php
@@ -18,8 +18,8 @@ class Sentinelone_model extends \Model
         $this->rs['last_seen'] = '';
         $this->rs['mgmt_url'] = '';
         $this->rs['self_protection_enabled'] = 0; //boolean
-        $this->rs['uuid'] '';
-        $this->rs['path'] '';
+        $this->rs['uuid'] = '';
+        $this->rs['path'] = '';
        
         if ($serial) {
             $this->retrieve_record($serial);
