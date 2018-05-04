9daa6bdcb654619c44e764cc49c4ba17c0e176da
eholtam <poundbangbash@users.noreply.github.com>
2018-05-03 21:25:56 -0500

    Add tab to provides

--- app/modules/sentinelonequarantine/provides.php
+++ app/modules/sentinelonequarantine/provides.php
@@ -1,6 +1,9 @@
 <?php
 
 return array(
+    'client_tabs' => array(
+      'sentinelonequarantine' => array('view' => 'sentinelone_quarantine_client_tab', 'i18n' => 'sentineloneuqarantine.sentinelone_quarantine'),
+    ),
     'listings' => array(
         'sentinelonequarantine' => array('view' => 'sentinelone_quarantine_listing', 'i18n' => 'sentinelone_quarantine.sentinelone_quarantine'),
     ),
