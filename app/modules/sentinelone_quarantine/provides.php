072238a5cab2679323bb0e8eccb6eab72ee9d436
eholtam <poundbangbash@users.noreply.github.com>
2018-05-03 20:39:40 -0500

    witchhunt

--- app/modules/sentinelone_quarantine/provides.php
+++ app/modules/sentinelone_quarantine/provides.php
@@ -2,6 +2,6 @@
 
 return array(
     'listings' => array(
-        'sentinelone_quarantine' => array('view' => 'sentinelone_quarantine_listing', 'i18n' => 'sentinelone_quarantine.sentinelone_quarantine'),
+        'sentinelonequarantine' => array('view' => 'sentinelone_quarantine_listing', 'i18n' => 'sentinelone_quarantine.sentinelone_quarantine'),
     ),
 );
