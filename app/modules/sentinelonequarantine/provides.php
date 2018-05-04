011630727530babd2662a4da50e0bd1d6a5c29ec
eholtam <poundbangbash@users.noreply.github.com>
2018-05-03 22:02:45 -0500

    add badge to cleint tab listing

--- app/modules/sentinelonequarantine/provides.php
+++ app/modules/sentinelonequarantine/provides.php
@@ -2,7 +2,7 @@
 
 return array(
     'client_tabs' => array(
-      'sentinelonequarantine' => array('view' => 'sentinelone_quarantine_client_tab', 'i18n' => 'sentinelonequarantine.sentinelone_quarantine'),
+      'sentinelonequarantine' => array('view' => 'sentinelone_quarantine_client_tab', 'i18n' => 'sentinelonequarantine.sentinelone_quarantine', 'badge' => 'sentinelonequarantine-cnt'),
     ),
     'listings' => array(
         'sentinelonequarantine' => array('view' => 'sentinelone_quarantine_listing', 'i18n' => 'sentinelonequarantine.sentinelone_quarantine'),
